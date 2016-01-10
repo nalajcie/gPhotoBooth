328ec4a3fc47ccf2a82969e3c3fdfe87b78f4e8b
nalajcie <nalajcie@gmail.com>
2016-01-10 11:39:57 +0000

    refactoring: MVC stub
     - Model-View-Controller stub
     - separate objects representing parts of GUI
     - Config object

--- photobooth.py
+++ photobooth.py
@@ -25,27 +25,161 @@ logger.addHandler(stdout_log_handler)
 logger.setLevel(logging.DEBUG)
 
 ### CONFGIURATION ###
-SCREEN_WIDTH  = 1280
-SCREEN_HEIGHT = 800
+# this is the default configuration, some values can be changed using commandline parameters
+default_config = {
+    # display-related consts
+    'screen_width': 1280,
+    'screen_height': 800,
+    'fullscreen': 0,
+    'fps': 10,
+
+    # controller-related vars
+    'save_path': '.',
+
+    # whole screen drawing-related consts
+    'font_color': (210, 210, 210),
+    'font_size': 142,
+    'back_color': (230, 180, 40),
+
+    'left_margin': 20,
+    'left_offset': 12,
+    'bottom_margin': 20,
+}
 
 
-class PhotoBooth(object):
-    def __init__(self, camera, image_dest, fullscreen, print_count, printer, upload_to):
+class PhotoPreview(pygame.sprite.Sprite):
+    """
+    Small static photo preview, displays empty frame at first, and a small picture afterwards
+    """
+    WIDTH  = 200
+    HEIGHT = 133
+    def __init__(self, group, number, conf):
+        pygame.sprite.Sprite.__init__(self, group)
+        self.conf = conf
+        self.number = number
+        # surface & positioning
+        self.image = pygame.Surface((PhotoPreview.WIDTH, PhotoPreview.HEIGHT)) # previews width/height
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (self.conf.left_margin + self.number * (PhotoPreview.WIDTH + self.conf.left_offset), self.conf.screen_height - self.conf.bottom_margin)
+
+        # drawing
+        pygame.draw.rect(self.image, (0,255,0), (0,0,PhotoPreview.WIDTH,PhotoPreview.HEIGHT),1)
+        #TODO: draw (number + 1)
+
+class PygView(object):
+    """
+    Main view which handles all of the rendering
+    """
+
+    CURSORKEYS = slice(273, 277)
+    QUIT_KEYS = pygame.K_ESCAPE, pygame.K_q
+    EVENTS = 'up', 'down', 'right', 'left'
+
+    def __init__(self, controller, conf):
+        self.conf = conf
+        self.controller = controller
+        self.fps = self.conf.fps
+
+        # create drawing components
+        self.previews_group = pygame.sprite.Group()
+        for num in xrange(4):
+            PhotoPreview(self.previews_group, num, conf)
+
+        pygame.init()
+        pygame.mouse.set_visible(False)
+
+        self.clock = pygame.time.Clock()
+
+        # TODO:
+        # self.add_button_listener()
+
+        flags = pygame.DOUBLEBUF | [0, pygame.FULLSCREEN][self.conf.fullscreen]
+        self.canvas = pygame.display.set_mode((self.conf.screen_width, self.conf.screen_height), flags)
+        self.font = pygame.font.SysFont(pygame.font.get_default_font(), self.conf.font_size)
+
+    def draw_text(self, text):
+
+        fw, fh = self.font.size(text)
+        surface = self.font.render(text, True, self.font_color)
+        self.canvas.blit(surface, ((self.width - fw) // 2, (self.height - fh) // 2))
+
+    def render_text_centred(self, *text_lines):
+        location = self.canvas.get_rect()
+        rendered_lines = [self.font.render(text, True, self.conf.font_color) for text in text_lines]
+        line_height = font.get_linesize()
+        middle_line = len(text_lines) / 2.0 - 0.5
+
+        for i, line in enumerate(rendered_lines):
+            line_pos = line.get_rect()
+            lines_to_shift = i - middle_line
+            line_pos.centerx = location.centerx
+            line_pos.centery = location.centery + lines_to_shift * line_height
+            self.main_surface.blit(line, line_pos)
+
+    def render_text_bottom(self, text, size=142):
+        location = self.canvas.get_rect()
+        font = pygame.font.SysFont(pygame.font.get_default_font(), size)
+        line = self.font.render(text, True, self.conf.font_color)
+        line_height = font.get_linesize()
+
+        line_pos = line.get_rect()
+        line_pos.centerx = location.centerx
+        line_pos.centery = location.height - 2 * line_height
+        self.main_surface.blit(line, line_pos)
+
+    def run(self):
+        """Main loop"""
+
+        running = True
+        while running:
+            self.clock.tick_busy_loop(self.fps)
+            running = self.controller.dispatch(self.get_events())
+            self.previews_group.draw(self.canvas)
+            self.flip()
+        else:
+            self.quit()
+
+
+    def get_events(self):
+
+        keys = pygame.key.get_pressed()[PygView.CURSORKEYS]
+        move_events = [e for e, k in zip(PygView.EVENTS, keys) if k]
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 'quit', move_events
+            if event.type == pygame.KEYDOWN:
+                if event.key in PygView.QUIT_KEYS:
+                    return 'quit', move_events
+                else:
+                    return 'other_key', move_events
+        else:
+            return None, move_events
+
+    def flip(self):
+        pygame.display.flip()
+        self.canvas.fill(self.conf.back_color)
+
+
+    def quit(self):
+        pygame.quit()
+
+
+class PhotoBoothController(object):
+    def __init__(self, camera, config):
         self.camera = camera
+        self.conf = config
+        self.state = 'playing'
 
         self.count_down_time = 5
         self.image_display_time = 3
         self.montage_display_time = 15
         self.idle_time = 240
 
-        self.print_count = print_count
-        self.printer = printer
-        self.upload_to = upload_to
-        self.output_dir = image_dest
-        self.size = None
-        self.fullscreen = fullscreen
-        self.events = []
         self.current_session = None
+
+        self.view = PygView(self, self.conf)
+
 
     def capture_preview(self):
         picture = self.camera.capture_preview()
@@ -64,30 +198,32 @@ class PhotoBooth(object):
         picture = pygame.transform.scale(picture, self.size)
         self.main_surface.blit(picture, (0, 0))
 
-    def start(self):
-        pygame.init()
-        pygame.mouse.set_visible(False)
+    def run(self):
+        self.view.run()
 
-        self.clock = pygame.time.Clock()
+    def dispatch(self, all_events):
+        """Control the game state."""
 
-        # TODO:
-        # self.add_button_listener()
+        event, move_events = all_events
+        if event == 'quit':
+            #self.game.quit()
+            return False
 
-        if self.fullscreen:
-            pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
-        else:
-            pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        if self.state == 'playing':
+            #self.state = self.game.process(self.view, move_events)
+            return True
 
+        if self.state == 'ending':
+            self.game.wait(self.view)
+            if event == 'other_key':
+                self.state = 'playing'
+                #self.game.reset(START)
 
-        self.main_surface = pygame.display.get_surface()
+        return True
 
-        self.size = self.main_surface.get_size()
-
-        while self.main_loop():
-            pass
-        self.camera.sleep()
 
     def main_loop(self):
+        #WARN: not used for now!
         pygame.event.clear()
         self.clock.tick(25)
         if len(self.events) > 10:
@@ -117,30 +253,6 @@ class PhotoBooth(object):
         self.main_surface.fill((0, 0, 0))
         self.render_text_centred('Press the button to start!')
 
-    def render_text_centred(self, *text_lines):
-        location = self.main_surface.get_rect()
-        font = pygame.font.SysFont(pygame.font.get_default_font(), 142)
-        rendered_lines = [font.render(text, 1, (210, 210, 210)) for text in text_lines]
-        line_height = font.get_linesize()
-        middle_line = len(text_lines) / 2.0 - 0.5
-
-        for i, line in enumerate(rendered_lines):
-            line_pos = line.get_rect()
-            lines_to_shift = i - middle_line
-            line_pos.centerx = location.centerx
-            line_pos.centery = location.centery + lines_to_shift * line_height
-            self.main_surface.blit(line, line_pos)
-
-    def render_text_bottom(self, text, size=142):
-        location = self.main_surface.get_rect()
-        font = pygame.font.SysFont(pygame.font.get_default_font(), size)
-        line = font.render(text, 1, (210, 210, 210))
-        line_height = font.get_linesize()
-
-        line_pos = line.get_rect()
-        line_pos.centerx = location.centerx
-        line_pos.centery = location.height - 2 * line_height
-        self.main_surface.blit(line, line_pos)
 
     def capture_image(self, file_name):
         file_path = os.path.join(self.output_dir, file_name)
@@ -164,10 +276,16 @@ class PhotoBooth(object):
         return not self.check_key_event(pygame.K_q, pygame.K_ESCAPE) \
             and not pygame.event.peek(pygame.QUIT)
 
+class Config(object):
+  """Change dictionary to object attributes."""
+
+  def __init__(self, **kwargs):
+
+    self.__dict__.update(kwargs)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("save_to", help="Location to save images")
+    parser.add_argument("save_path", help="Location to save images")
     parser.add_argument("-d", "--dummy",        help="Use dummy camera instead of GPhoto interface", action="store_true")
     parser.add_argument("-f", "--fullscreen",   help="Use fullscreen mode", action="store_true")
     parser.add_argument("-p", "--print_count",  help="Set number of copies to print", type=int, default=0)
@@ -176,21 +294,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     logger.info("Args were: %s", args)
-
+    conf = Config(**default_config)
+    conf.fullscreen = args.fullscreen
+    conf.save_path = args.save_path
+    logger.info("Full configuration: %s", conf)
 
     if args.dummy:
         camera = DummyCamera()
     else:
         camera = GPhotoCamera()
 
-    booth = PhotoBooth(camera,
-                       args.save_to,
-                       fullscreen=(args.fullscreen),
-                       print_count=args.print_count,
-                       printer=args.printer,
-                       upload_to=args.upload_to)
+    booth = PhotoBoothController(camera, conf)
     try:
-        booth.start()
+        booth.run()
     except Exception:
         logger.exception("Unhandled exception!")
         camera.close()
