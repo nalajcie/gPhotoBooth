c681c2f3bfb3062a61592f89beeef81513c8ed5b
nalajcie <nalajcie@gmail.com>
2016-01-13 21:11:10 +0000

    camera: check if camera can be initialised, capture_image

--- camera.py
+++ camera.py
@@ -2,10 +2,15 @@ from local_modules import piggyphoto
 from StringIO import StringIO
 
 import pygame
+import shutil
 
 class GPhotoCamera():
     def __init__(self):
-        self.cam = piggyphoto.Camera()
+        try:
+            self.cam = piggyphoto.Camera()
+        except piggyphoto.libgphoto2error, e:
+            raise ValueError("GPhotoCamera could not be initialised: %s" % e.message)
+        print "CAMERA: %s " % self.cam.abilities
 
     def capture_preview(self):
         cfile = self.cam.capture_preview()
@@ -13,16 +18,25 @@ class GPhotoCamera():
         cfile.clean()
         return picture
 
+    def capture_image(self, file_path):
+        self.cam.capture_image(file_path)
+        return
+
     def close(self):
         self.cam.close()
 
 class DummyCamera():
     def __init__(self):
+        print "CAMERA: DummyCamera serving only static JPEGs"
         return
 
     def capture_preview(self):
         picture = pygame.image.load("dummy-preview.jpg")
         return picture
+
+    def capture_image(self, file_path):
+        shutil.copyfile("dummy-preview.jpg", file_path)
+        return
 
     def close(self):
         return
