cf7c7c44c7bbfe7b03c2b598944124b8d90067c9
nalajcie <nalajcie@gmail.com>
2016-01-17 20:31:34 +0000

    configuration: add debug conf

--- photobooth.py
+++ photobooth.py
@@ -5,7 +5,7 @@ import sys
 import argparse
 import logging
 
-from photobooth import controller, camera
+from photobooth import controller, camera, config
 
 
 
@@ -25,53 +25,12 @@ logger.addHandler(stdout_log_handler)
 
 logger.setLevel(logging.DEBUG)
 
-### CONFGIURATION ###
-# this is the default configuration, some values can be changed using commandline parameters
-default_config = {
-    # display-related consts
-    'screen_width': 1280,
-    'screen_height': 800,
-    'fullscreen': 0,
-    'fps': 30,
-
-    # controller-related vars
-    'save_path': '.',
-    'flip_preview': True,
-    'initial_countdown_secs': 3,
-    'midphoto_countdown_secs': 3,
-    'image_display_secs': 2,
-    'montage_display_secs': 5,
-    'idle_secs': 30,
-    'montage_fps': 4,
-
-    # whole screen drawing-related consts
-    'font_color': (210, 210, 210),
-    'font_size': 142,
-    'back_color': (230, 180, 40),
-    'back_image': 'assets/pixelbackground_02_by_kara1984.jpg',
-
-    'left_margin': 20,
-    'left_offset': 48/3,
-    'bottom_margin': 20,
-    'top_margin': 20,
-}
-
-
-
-
-class Config(object):
-    """Change dictionary to object attributes."""
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __str__(self):
-        return str(self.__dict__)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("save_path", help="Location to save images")
     parser.add_argument("-d", "--dummy",        help="Use dummy camera instead of GPhoto interface", action="store_true")
+    parser.add_argument("-D", "--debug",        help="Use debug configuration for easier development", action="store_true")
     parser.add_argument("-f", "--fullscreen",   help="Use fullscreen mode", action="store_true")
     parser.add_argument("-p", "--print_count",  help="Set number of copies to print", type=int, default=0)
     parser.add_argument("-P", "--printer",      help="Set printer to use", default=None)
@@ -79,7 +38,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     logger.info("Args were: %s", args)
-    conf = Config(**default_config)
+    if args.debug:
+        conf = config.Config.debug()
+    else:
+        conf = config.Config.default()
+
     conf.fullscreen = args.fullscreen
     conf.save_path = args.save_path
     logger.info("Full configuration: %s", conf)
---
+++ photobooth/config.py
@@ -0,0 +1,63 @@
+
+
+### CONFGIURATION ###
+# this is the default configuration, some values can be changed using commandline parameters
+default_config = {
+    # display-related consts
+    'screen_width': 1280,
+    'screen_height': 800,
+    'fullscreen': 0,
+    'fps': 30,
+
+    # controller-related vars
+    'save_path': '.',
+    'flip_preview': True,
+    'initial_countdown_secs': 3,
+    'midphoto_countdown_secs': 3,
+    'image_display_secs': 2,
+    'montage_display_secs': 5,
+    'idle_secs': 30,
+    'montage_fps': 4,
+
+    # whole screen drawing-related consts
+    'font_color': (210, 210, 210),
+    'font_size': 142,
+    'back_color': (230, 180, 40),
+    'back_image': 'assets/pixelbackground_02_by_kara1984.jpg',
+
+    'left_margin': 20,
+    'left_offset': 48/3,
+    'bottom_margin': 20,
+    'top_margin': 20,
+}
+
+debug_override = {
+    'initial_countdown_secs': 1,
+    'midphoto_countdown_secs': 1,
+    'image_display_secs': 1,
+    'montage_display_secs': 5,
+    'idle_secs': 30,
+    'montage_fps': 4,
+}
+
+
+
+
+class Config(object):
+    """Change dictionary to object attributes."""
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    @classmethod
+    def default(cls):
+        return cls(**default_config)
+
+    @classmethod
+    def debug(cls):
+        conf = cls(**default_config)
+        conf.__dict__.update(**debug_override)
+        return conf
