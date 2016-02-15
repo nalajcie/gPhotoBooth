516ca5b2434048c5da6771cae2a69f9ab37c8f92
White Moustache <nalajcie@gmail.com>
2016-02-15 21:55:49 +0000

    dev: dimming button using hardware PWM (wiringpi lib)

--- dev/dimming_button_wiring.py
+++ dev/dimming_button_wiring.py
@@ -13,17 +13,28 @@ LED_DUTY_MAX = 1024
 LED_DUTY_MIN = 128
 LED_DUTY_STEP= 16
 
-BUTTON_PIN = 16
+BUTTON_PIN = 23
+
+DEBOUNCE_MS = 20
 
 #WARN: the below line requires root
 wiringpi2.wiringPiSetupGpio()
-wiringpi2.pinMode(LED_PIN, 2)  # hardware PWM mode
+wiringpi2.pinMode(LED_PIN, wiringpi2.GPIO.PWM_OUTPUT)  # hardware PWM mode
 
+count = 0
 
-def button_raw_press(channel):
-    print "BUTTON PRESS!"
+def button_raw_press():
+    # simple debounce with busy-waiting
+    time.sleep(DEBOUNCE_MS // 1000)
+    if not wiringpi2.digitalRead(BUTTON_PIN):
+        global count
+        count += 1
+        print("BUTTON PRESS: %d" % (count))
 
-#GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_raw_press, bouncetime=500)
+wiringpi2.pinMode(BUTTON_PIN, wiringpi2.GPIO.INPUT)
+wiringpi2.pullUpDnControl(BUTTON_PIN, wiringpi2.GPIO.PUD_UP)
+
+wiringpi2.wiringPiISR(BUTTON_PIN, wiringpi2.GPIO.INT_EDGE_BOTH, button_raw_press)
 
 try:
     while True:
