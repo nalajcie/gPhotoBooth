98889361ebbe091eb533fce225f7d1a2550b2b6d
nalajcie <nalajcie@gmail.com>
2016-02-18 07:52:35 +0000

    pi: because of hardware PWM the app requires sudo to be run

--- pi-run.sh
+++ pi-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh pi@raspberrypi.local "killall python; cd src/gPhotoBooth; DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -f xxx"
+ssh pi@raspberrypi.local "sudo killall python; cd src/gPhotoBooth; sudo DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -f xxx -P"
 
