ff0de4147b36022997a40bb3e80374b077f7ff04
nalajcie <nalajcie@gmail.com>
2016-02-18 21:04:35 +0000

    dev: experimenting with creating GIFs

--- dev/create_photo_sess_gif.py
+++ dev/create_photo_sess_gif.py
@@ -2,9 +2,20 @@
 import images2gif
 import PIL
 import sys
+import time
+import subprocess
 
+img_list = [ ("../tmp/" + sys.argv[1] +"/" + str(i) + "_medium.jpg") for i in xrange(1, 5) ]
 
-l = [ PIL.Image.open("../tmp/" + sys.argv[1] +"/" + str(i) + "_prev.jpg") for i in xrange(1, 5) ]
+start = time.time()
+l = [ PIL.Image.open(f) for f in img_list ]
+images2gif.writeGif("py.gif", l, duration=0.2)
+print "PYTHON GIF time = %f" % (time.time() - start)
 
-images2gif.writeGif("py.gif", l, duration=0.5)
-
+start = time.time()
+cmd = ["convert", "-delay", "20", "-loop", "0"]
+cmd.extend(img_list)
+cmd.append("out.gif")
+print cmd
+subprocess.call(cmd)
+print "SUBSHELL GIF time = %f" % (time.time() - start)
