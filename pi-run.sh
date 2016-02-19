35d754b5444227448d129bc792d29c806a7f1710
nalajcie <nalajcie@gmail.com>
2016-02-19 08:15:55 +0000

    pi-run: upload by default

--- pi-run.sh
+++ pi-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh pi@raspberrypi.local "sudo killall python; cd src/gPhotoBooth; sudo DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -f xxx -P"
+ssh pi@raspberrypi.local "sudo killall python; cd src/gPhotoBooth; sudo DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -u -f xxx -P"
 
