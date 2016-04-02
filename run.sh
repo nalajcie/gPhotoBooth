29c936968b302b7388617abedb42d07cf8f27f0e
White Moustache <nalajcie@gmail.com>
2016-04-02 20:42:26 +0000

    target run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# NOTE: for auto-start add this script to ~/.config/lxsession/LXDE-pi/autostart
+
+cd ~/src/gPhotoBooth/
+sudo DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -u -f xxx -P
