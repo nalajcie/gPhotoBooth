8f22f71183faa42250655ba5b0a96f0573422058
nalajcie <nalajcie@gmail.com>
2016-04-12 22:09:04 +0100

    update deployment script

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -3,5 +3,7 @@
 scp photobooth/platform/*.py pi@raspberrypi.local:src/gPhotoBooth/photobooth/platform/
 scp photobooth/*.py pi@raspberrypi.local:src/gPhotoBooth/photobooth/
 scp photobooth.py pi@raspberrypi.local:src/gPhotoBooth/
+scp events/template/config.yaml pi@raspberrypi.local:src/gPhotoBooth/events/template/
+scp events/pi/config.yaml pi@raspberrypi.local:src/gPhotoBooth/events/xxx/
 
 
