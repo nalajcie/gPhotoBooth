a19751c7fa55c99bd831b8665bb6ab254b965eb5
nalajcie <nalajcie@gmail.com>
2016-04-20 00:13:00 +0100

    assets: add sample event logo

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -2,6 +2,7 @@
 
 scp photobooth/platform/*.py pi@raspberrypi.local:src/gPhotoBooth/photobooth/platform/
 scp photobooth/*.py pi@raspberrypi.local:src/gPhotoBooth/photobooth/
+scp assets/* pi@raspberrypi.local:src/gPhotoBooth/assets/
 scp photobooth.py pi@raspberrypi.local:src/gPhotoBooth/
 scp events/template/config.yaml pi@raspberrypi.local:src/gPhotoBooth/events/template/
 scp events/pi/config.yaml pi@raspberrypi.local:src/gPhotoBooth/events/xxx/
