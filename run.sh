7adce2d873ea056a4b6e87b326601f0398797f7d
White Moustache <nalajcie@gmail.com>
2016-05-17 20:59:07 +0200

    run.sh: when exiting by user input, do not restart

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@
 cd ~/src/gPhotoBooth/
 while :; do
     sudo DISPLAY=:0 LD_LIBRARY_PATH=/home/pi/turbo/optimized/lib/ python ./photobooth.py -f events/xxx
+    EXIT_CODE=$?
     # shoo, zombie processes
     sudo killall python
+    if [ $EXIT_CODE -eq 0 ]; then
+        echo "Exited normally, ending loop";
+        break;
+    fi
 done
